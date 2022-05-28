1d5b86c62c6b0b579114d7111ed9964bbd323495
HydroCarbon <2728292961@qq.com>
2022-05-28 23:53:44 +0800

    Initial commit

---
+++ algorithm-leetcode/src/main/java/org/hydrocarbonx/algorithm/leetcode/Main.java
@@ -0,0 +1,14 @@
+package org.hydrocarbonx.algorithm.leetcode;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author HydroCarbon
+ * @since 2022-05-28
+ */
+@Slf4j
+public class Main {
+    public static void main(String[] args) {
+        log.info("Hello algorithm of leetcode!");
+    }
+}
