697a2dc16bdd9595c2d970910652fc98bcb74210
HydroCarbon <2728292961@qq.com>
2022-06-22 11:45:36 +0800

    Add leetcode 513

---
+++ algorithm-leetcode/algorithm-leetcode-medium/src/test/java/org/hydrocarbonx/algorithm/leetcode/medium/_513FindBottomLeftTreeValueTest.java
@@ -0,0 +1,39 @@
+package org.hydrocarbonx.algorithm.leetcode.medium;
+
+import lombok.extern.slf4j.Slf4j;
+import org.hydrocarbonx.algorithm.leetcode.common.TreeNode;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author HydroCarbon
+ * @since 2022-06-22
+ */
+@Slf4j
+@SuppressWarnings("all")
+class _513FindBottomLeftTreeValueTest {
+
+    @Test
+    void findBottomLeftValue() {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+
+        int result = new _513FindBottomLeftTreeValue().findBottomLeftValue(root);
+        Assertions.assertEquals(2, result);
+
+        root.left.left = new TreeNode(4);
+
+        result = new _513FindBottomLeftTreeValue().findBottomLeftValue(root);
+        Assertions.assertEquals(4, result);
+
+        root.right.left = new TreeNode(5);
+        root.right.right = new TreeNode(6);
+        root.right.left.left = new TreeNode(7);
+
+        result = new _513FindBottomLeftTreeValue().findBottomLeftValue(root);
+        Assertions.assertEquals(7, result);
+    }
+}
