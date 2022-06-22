e79fc03bfac1138e43046256762f0bfe779e7500
HydroCarbon <2728292961@qq.com>
2022-06-22 14:50:37 +0800

    Add leetcode 2210

---
+++ algorithm-leetcode/algorithm-leetcode-easy/src/test/java/org/hydrocarbonx/algorithm/leetcode/easy/_2210CountHillsAndValleysInAnArrayTest.java
@@ -0,0 +1,27 @@
+package org.hydrocarbonx.algorithm.leetcode.easy;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author HydroCarbon
+ * @since 2022-06-22
+ */
+@Slf4j
+@SuppressWarnings("all")
+class _2210CountHillsAndValleysInAnArrayTest {
+
+    @Test
+    void countHillValley() {
+        int[] nums = new int[]{2, 4, 1, 1, 6, 5};
+        int result = new _2210CountHillsAndValleysInAnArray().countHillValley(nums);
+        Assertions.assertEquals(3, result);
+
+        nums = new int[]{6, 6, 5, 5, 4, 1};
+        result = new _2210CountHillsAndValleysInAnArray().countHillValley(nums);
+        Assertions.assertEquals(0, result);
+    }
+}
